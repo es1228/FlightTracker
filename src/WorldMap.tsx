3843aba25b2361154a942b781f5c97050d19127d
Evan Sidhu <129010227+es1228@users.noreply.github.com>
2026-06-06 17:07:07 -0400

    feat: add stadia basemaps for light, dark, and satellite

--- src/WorldMap.tsx
+++ src/WorldMap.tsx
@@ -1,10 +1,38 @@
-import { MapContainer, TileLayer } from "react-leaflet";
-import 'leaflet/dist/leaflet.css';
+import {
+	LayerGroup,
+	LayersControl,
+	MapContainer,
+	TileLayer,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const WorldMap = () => {
 	return (
-		<MapContainer center={[0, 0]} zoom={3} minZoom={3} maxZoom={18} maxBounds={[[-85, -Infinity], [85, Infinity]]} maxBoundsViscosity={1.0}>
-			<TileLayer url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}" />
+		<MapContainer
+			center={[0, 0]}
+			zoom={3}
+			minZoom={3}
+			maxZoom={18}
+			maxBounds={[
+				[-85, -Infinity],
+				[85, Infinity],
+			]}
+			maxBoundsViscosity={1.0}
+		>
+			<LayersControl>
+				<LayersControl.BaseLayer checked name="Satellite">
+					<LayerGroup>
+						<TileLayer
+							url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png" />
+					</LayerGroup>
+				</LayersControl.BaseLayer>
+				<LayersControl.BaseLayer name="Light">
+					<TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png" />
+				</LayersControl.BaseLayer>
+				<LayersControl.BaseLayer name="Dark">
+					<TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" />
+				</LayersControl.BaseLayer>
+			</LayersControl>
 		</MapContainer>
 	);
 };
